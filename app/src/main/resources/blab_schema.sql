cc4b07ec63648bc48bff5f98c49d5dc17ff1d7f7
Ritu786 <65646433+Ritu786@users.noreply.github.com>
2024-07-11 16:04:17 +0530

    Update blab_schema.sql

--- app/src/main/resources/blab_schema.sql
+++ app/src/main/resources/blab_schema.sql
@@ -27,7 +27,7 @@ CREATE TABLE listeners (
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
   username varchar(100) NOT NULL,
-  password varchar(100) DEFAULT NULL,
+  password varchar(100) DEFAULT NULL,  -- Consider using a secure hashing mechanism for password storage.
   password_hint varchar(100) DEFAULT NULL,
   created_at datetime DEFAULT NULL,
   last_login datetime DEFAULT NULL,
