98c1912a6ed27aca78e1b76f27999061278e165d
Ritu786 <65646433+Ritu786@users.noreply.github.com>
2024-07-11 14:56:31 +0530

    Update blab_schema.sql

--- app/src/main/resources/blab_schema.sql
+++ app/src/main/resources/blab_schema.sql
@@ -16,84 +16,84 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `blabs`
+-- Table structure for table "blabs"
 --
 
-DROP TABLE IF EXISTS `blabs`;
+DROP TABLE IF EXISTS "blabs";
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `blabs` (
-  `blabid` int(11) NOT NULL AUTO_INCREMENT,
-  `blabber` varchar(100) NOT NULL,
-  `content` varchar(250) DEFAULT NULL,
-  `timestamp` datetime DEFAULT NULL,
-  PRIMARY KEY (`blabid`)
+CREATE TABLE "blabs" (
+  "blabid" int(11) NOT NULL AUTO_INCREMENT,
+  "blabber" varchar(100) NOT NULL,
+  "content" varchar(250) DEFAULT NULL,
+  "timestamp" datetime DEFAULT NULL,
+  PRIMARY KEY ("blabid")
 ) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `comments`
+-- Table structure for table "comments"
 --
 
-DROP TABLE IF EXISTS `comments`;
+DROP TABLE IF EXISTS "comments";
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `comments` (
-  `commentid` int(11) NOT NULL AUTO_INCREMENT,
-  `blabid` int(11) NOT NULL,
-  `blabber` varchar(100) NOT NULL,
-  `content` varchar(250) DEFAULT NULL,
-  `timestamp` datetime DEFAULT NULL,
-  PRIMARY KEY (`commentid`)
+CREATE TABLE "comments" (
+  "commentid" int(11) NOT NULL AUTO_INCREMENT,
+  "blabid" int(11) NOT NULL,
+  "blabber" varchar(100) NOT NULL,
+  "content" varchar(250) DEFAULT NULL,
+  "timestamp" datetime DEFAULT NULL,
+  PRIMARY KEY ("commentid")
 ) ENGINE=InnoDB AUTO_INCREMENT=166 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `listeners`
+-- Table structure for table "listeners"
 --
 
-DROP TABLE IF EXISTS `listeners`;
+DROP TABLE IF EXISTS "listeners";
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `listeners` (
-  `blabber` varchar(100) NOT NULL,
-  `listener` varchar(100) NOT NULL,
-  `status` varchar(20) DEFAULT NULL
+CREATE TABLE "listeners" (
+  "blabber" varchar(100) NOT NULL,
+  "listener" varchar(100) NOT NULL,
+  "status" varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `users`
+-- Table structure for table "users"
 --
 
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS "users";
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users` (
-  `username` varchar(100) NOT NULL,
-  `password` varchar(100) DEFAULT NULL,
-  `password_hint` varchar(100) DEFAULT NULL,
-  `created_at` datetime DEFAULT NULL,
-  `last_login` datetime DEFAULT NULL,
-  `real_name` varchar(100) DEFAULT NULL,
-  `blab_name` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`username`)
+CREATE TABLE "users" (
+  "username" varchar(100) NOT NULL,
+  "password" varchar(100) DEFAULT NULL,
+  "password_hint" varchar(100) DEFAULT NULL,
+  "created_at" datetime DEFAULT NULL,
+  "last_login" datetime DEFAULT NULL,
+  "real_name" varchar(100) DEFAULT NULL,
+  "blab_name" varchar(100) DEFAULT NULL,
+  PRIMARY KEY ("username")
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `users_history`
+-- Table structure for table "users_history"
 --
 
-DROP TABLE IF EXISTS `users_history`;
+DROP TABLE IF EXISTS "users_history";
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `users_history` (
-  `eventid` int(11) NOT NULL AUTO_INCREMENT,
-  `blabber` varchar(100) NOT NULL,
-  `event` varchar(250) DEFAULT NULL,
-  `timestamp` datetime DEFAULT NULL,
-  PRIMARY KEY (`eventid`)
+CREATE TABLE "users_history" (
+  "eventid" int(11) NOT NULL AUTO_INCREMENT,
+  "blabber" varchar(100) NOT NULL,
+  "event" varchar(250) DEFAULT NULL,
+  "timestamp" datetime DEFAULT NULL,
+  PRIMARY KEY ("eventid")
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
